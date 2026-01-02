336b3381a9a5d9d9d14d0c37bce97dbbdad6a494
VSA1GraveDigger <333amit7777777@gmail.com>
2026-01-02 14:39:10 +0530

    Adding twoSum problem.

---
+++ 62twoSum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<iterator>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>x;
+        bool flag = false;
+        if(flag == false && nums.size() > 1)
+        for(int i = 0; i < nums.size(); i++){
+            if(flag == false)
+            for(int j = 1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    if(i != j){
+                        x.push_back(i);
+                    x.push_back(j);
+                    flag = true;
+                    }
+                }
+            }
+       }
+       else
+       return nums;
+       return x;
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int> x = {2,3,4,5,6,1} ,y;
+    y = obj.twoSum(x, 9);
+    for(vector<int>::iterator it = y.begin(); it !=y.end(); it++){
+        cout<<*it;
+    }
+return 0;
+}
