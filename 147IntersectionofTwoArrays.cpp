a02631673a78e4d06231c9c752dde537c19f65c2
VSA1GraveDigger <333amit7777777@gmail.com>
2026-03-23 21:23:50 +0530

    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.

---
+++ 147IntersectionofTwoArrays.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<set>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int>result;
+        for(auto x : nums1){
+            for(auto y : nums2){
+                if(x == y){
+                    result.insert(x);
+                }
+            }
+        }
+        vector<int> ans(result.begin(), result.end());
+        return ans;
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int>nums1 = {4,9,5};
+    vector<int>nums2 = {9,4,9,8,4};
+    vector<int>result = obj.intersection(nums1, nums2); cout<<"Intersection of two integer arrays : ";
+    for(auto x : result){
+        cout<<x<<" ";
+    }
+    return 0;
+}
