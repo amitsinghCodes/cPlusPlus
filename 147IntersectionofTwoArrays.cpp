7751fb445353712ba9a443390c1f498264becbb3
VSA1GraveDigger <333amit7777777@gmail.com>
2026-03-23 21:24:45 +0530

    ipdated cpp

--- 147IntersectionofTwoArrays.cpp
+++ 147IntersectionofTwoArrays.cpp
@@ -18,7 +18,23 @@ public:
         return ans;
     }
 };
+/*
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s(nums1.begin(), nums1.end());
+        unordered_set<int> result;
 
+        for (int x : nums2) {
+            if (s.count(x)) {
+                result.insert(x);
+            }
+        }
+
+        return vector<int>(result.begin(), result.end());
+    }
+};
+*/
 int main(){
     Solution obj;
     vector<int>nums1 = {4,9,5};
