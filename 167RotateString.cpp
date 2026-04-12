8907654a4c5a6fcc32284e30adf08b9f7d967f11
VSA1GraveDigger <333amit7777777@gmail.com>
2026-04-12 22:48:04 +0530

    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.

---
+++ 167RotateString.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        return s.size() == goal.size() && (s + s).find(goal) != string::npos;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s = "abcde";
+    string goal = "cdeab";
+
+    cout << (sol.rotateString(s, goal) ? "true" : "false") << endl;
+
+    return 0;
+}
