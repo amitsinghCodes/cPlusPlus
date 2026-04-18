cf60f87e0c765addd41484df2d1a63ce51de68e2
VSA1GraveDigger <333amit7777777@gmail.com>
2026-04-18 11:02:11 +0530

    A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
    Given an integer n, return true if n is a perfect number, otherwise return false.

---
+++ 172PerfectNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 0; 
+        for(int i = 1; i <= num/2; i++){
+            if(num % i == 0){
+                sum += i;
+            }
+        }
+        return sum == num ? true : false;
+    }
+};
+
+int main(){
+    Solution obj;
+    cout<<boolalpha;
+    cout<<obj.checkPerfectNumber(28);
+    return 0;
+}
