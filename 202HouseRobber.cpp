b395ba099e75201665476b0d6c153913b459d56e
VSA1GraveDigger <333amit7777777@gmail.com>
2026-05-18 22:05:57 +0530

    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.

---
+++ 202HouseRobber.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int money : nums) {
+            int take = money + prev2;
+            int skip = prev1;
+
+            int curr = max(take, skip);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 9, 3, 1};
+
+    Solution sol;
+    cout << sol.rob(nums) << endl;
+
+    return 0;
+}
