eb7307de5cdb8591cd27e247b9ea56928d40700a
VSA1GraveDigger <333amit7777777@gmail.com>
2026-06-03 23:01:52 +0530

    Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.

---
+++ 218RemoveKDigits.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+        string st;
+
+        for(char c : num) {
+
+            while(!st.empty() &&
+                  k > 0 &&
+                  st.back() > c) {
+                st.pop_back();
+                k--;
+            }
+
+            st.push_back(c);
+        }
+
+        while(k > 0 && !st.empty()) {
+            st.pop_back();
+            k--;
+        }
+
+        int i = 0;
+        while(i < st.size() && st[i] == '0')
+            i++;
+
+        string ans = st.substr(i);
+
+        return ans.empty() ? "0" : ans;
+    }
+};
+
+int main() {
+
+    Solution sol;
+
+    string num = "1432219";
+    int k = 3;
+
+    cout << sol.removeKdigits(num, k) << endl;
+
+    return 0;
+}
