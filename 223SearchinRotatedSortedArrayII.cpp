a251bdacab2f676bd42dcc6d533ae9b71ec2f6e0
VSA1GraveDigger <333amit7777777@gmail.com>
2026-06-08 22:28:43 +0530

    There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
    Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
    
    Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
    
    You must decrease the overall operation steps as much as possible.

---
+++ 223SearchinRotatedSortedArrayII.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return true;
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    vector<int> nums = {2,5,6,0,0,1,2};
+
+    Solution sol;
+
+    cout << sol.search(nums, 0) << endl; // 1
+    cout << sol.search(nums, 3) << endl; // 0
+
+    return 0;
+}
